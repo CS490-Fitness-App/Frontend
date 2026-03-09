64bc319d16390b509eb44d46b37366cd32b5a84f
Hira :) <hirashakir10@gmail.com>
2026-03-09 02:41:52 -0400

    working on login and create account page

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,85 @@
+import "./LoginForm.css"
+import React from 'react'
+import { MdEmail } from "react-icons/md";
+import { IoMdLock } from "react-icons/io";
+import { FaGoogle } from "react-icons/fa";
+import { MdCancel } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+
+export const LoginForm = () => {
+    return (
+        <div class="modal-container">
+            <div class="modal-content">
+                <MdCancel class="cancel" />
+
+                <form class="login">
+                    <h2>Login</h2>
+
+                    <div class="form-control">
+                        <label for="email"><MdEmail /> Email</label>
+                        <input type="email" id="email" placeholder="Enter your email" />
+                    </div>
+
+                    <div class="form-control">
+                        <label for="password"><IoMdLock /> Password</label>
+                        <input type="password" id="password" placeholder="Enter your password" />
+                    </div>
+                    <div class="form-control-others">
+                        <button>Login</button>
+                    </div>
+                    <div class="form-control-others">
+                        <p> -- Or sign in with -- </p>
+                        <button><FaGoogle /> Google</button>
+                    </div>
+                    <div class="form-control-others">
+                        <label for="create">
+                            Don't have an account? <a href="javascript:void(0)" id="Login">Sign up now</a>
+                        </label>
+                    </div>
+                </form>
+
+                <form class="registration">
+                    <h2>Create Account</h2>
+
+                    <div class="horiz-form">
+                        <div class="form-control">
+                            <label for="first-name"><FaUser /> First Name</label>
+                            <input type="first-name" id="first-name" placeholder="Enter your first name" />
+                        </div>
+                        <div class="form-control">
+                            <label for="last-name"><FaUser /> Last Name</label>
+                            <input type="last-name" id="last-name" placeholder="Enter your last name" />
+                        </div>
+                    </div>
+
+                    <div class="form-control">
+                        <label for="email"><MdEmail /> Email</label>
+                        <input type="email" id="email" placeholder="Enter your email" />
+                    </div>
+
+                    <div class="form-control">
+                        <label for="password"><IoMdLock /> Password</label>
+                        <input type="password" id="password" placeholder="Enter your password" />
+                    </div>
+                    <div class="form-control">
+                        <label for="password"><IoMdLock /> Confirm Password</label>
+                        <input type="password" id="password" placeholder="Confirm your password" />
+                    </div>
+
+                    <div class="form-control-others">
+                        <button>Create Account</button>
+                    </div>
+                    <div class="form-control-others">
+                        <p> -- Or sign in with -- </p>
+                        <button><FaGoogle /> Google</button>
+                    </div>
+                    <div class="form-control-others">
+                        <label for="login">
+                            Already have an account? <a href="javascript:void(0)" id="Login">Log in</a>
+                        </label>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,30 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom' 
 import { IoFitness } from "react-icons/io5";
 
+import { LoginForm } from "./LoginForm"
+
 export const Navbar = () => {
     return (
-        <nav>
-                
-            <Link to="/" className="title">
-                <IoFitness/>  Primal Fitness
-            </Link>
-            <ul>
-                <li>
-                    <NavLink to="/exercises">Exercises</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/workouts">Workouts</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">Log In</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav>          
+                <Link to="/" class="logo">
+                    <IoFitness/>  Primal Fitness
+                </Link>
+                <ul>
+                    <li>
+                        <NavLink to="/exercises">Exercises</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/workouts">Workouts</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">Log In</NavLink>
+                    </li>
+                </ul>
+            </nav>
+
+            <LoginForm />
+        </div>
+
     )
 }
