f5d6773358ae1015c46e9307b3575100a5332ace
nandika-k <nkarnik5@outlook.com>
2026-03-28 20:10:13 -0400

    exercise cards

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,16 +1,17 @@
 import "./ExerciseCard.css"
 import React from 'react'
-import { Link } from 'react-router-dom' 
 
-export const ExerciseCard = ({ onClick }) => {
+export const ExerciseCard = ({ exercise, onClick }) => {
     return (
         <div className="card-container" onClick={onClick} style={{ cursor: 'pointer' }}>
-            <img className="card-img" src="https://picsum.photos/300/200" alt="Exercise Image" />
-            <div className="card-tag">Beginner</div>
-            <h2 className="card-title">Dumbbell Lateral Raise</h2>
-
-            {/*<Link className="card-btn" onClick={(e) => { e.preventDefault(); onClick() }}>View Exercise</Link>*/}
-            <Link className="card-btn">View Exercise</Link>
+            <img
+                className="card-img"
+                src={exercise.image_url || 'https://picsum.photos/300/200'}
+                alt={exercise.name}
+            />
+            <div className="card-tag">{exercise.experience_level || 'N/A'}</div>
+            <h2 className="card-title">{exercise.name}</h2>
+            <span className="card-btn">View Exercise</span>
         </div>
     )
 }
