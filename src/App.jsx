22bfccb7f5047be367fa7f656e14f416e7ee2d69
rink-sys <rinkushah1201@gmail.com>
2026-04-08 03:02:53 -0400

    Integrate workout view, edit, and calendar pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { AuthSync } from './components/AuthSync'
 
-
-import { Home, Exercises, Workouts, Login, ClientDashboard, CoachDashboard, AdminDashboard, SignUp, Survey, Coaches } from './pages'
+import { Home, Exercises, Workouts, Login, ClientDashboard, SignUp, Survey, ViewWorkout, EditWorkout, ClientCalendar } from './pages'
+import { CalendarComponent } from './components/CalendarComponent'
 
 function App() {
     return (
@@ -14,14 +14,14 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/exercises" element={<Exercises />} />
-                <Route path="/coaches" element={<Coaches/>} />
                 <Route path="/workouts" element={<Workouts />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/client-dashboard" element={<ClientDashboard />} />
-                <Route path="/coach-dashboard" element={<CoachDashboard />} />
-                <Route path="/admin-dashboard" element={<AdminDashboard />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/survey" element={<Survey />} />
+                <Route path="/view-workout/:workoutId" element={<ViewWorkout />} />
+                <Route path="/edit-workout/:workoutId" element={<EditWorkout />} />
+                <Route path="/calendar" element={<ClientCalendar />} />
             </Routes>
         </div>
     )
