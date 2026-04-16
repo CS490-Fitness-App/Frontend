4caa04bbbf5fb99cb1f8aa1ef70545666994873d
nandika-k <nkarnik5@outlook.com>
2026-04-16 15:30:47 -0400

    avoid signout on reload

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,10 +3,16 @@ import React, { createContext, useContext, useState } from 'react'
 const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
-    const [customAuth, setCustomAuth] = useState(null)
+    const [customAuth, setCustomAuth] = useState(() => localStorage.getItem('customAuth'))
 
-    const setAuth = (token) => setCustomAuth(token)
-    const clearAuth = () => setCustomAuth(null)
+    const setAuth = (token) => {
+        localStorage.setItem('customAuth', token)
+        setCustomAuth(token)
+    }
+    const clearAuth = () => {
+        localStorage.removeItem('customAuth')
+        setCustomAuth(null)
+    }
 
     return (
         <AuthContext.Provider value={{ customAuth, setAuth, clearAuth }}>
